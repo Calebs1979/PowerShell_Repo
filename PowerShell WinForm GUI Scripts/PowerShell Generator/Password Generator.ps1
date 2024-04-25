df632e97d49b96c2b0b3dcbdf0a07f9ceb98f484
Github_with_Smitti <caleb.s1979@gmail.com>
2024-04-25 07:44:47 +0200

    Update Password Generator.ps1

--- PowerShell WinForm GUI Scripts/PowerShell Generator/Password Generator.ps1
+++ PowerShell WinForm GUI Scripts/PowerShell Generator/Password Generator.ps1
@@ -150,7 +150,7 @@ function Create-GUI {
     # Create Picture Box
     $pictureBox = New-Object System.Windows.Forms.PictureBox
     $pictureBox.Location = New-Object System.Drawing.Point(250, 70)
-    $imageURL = "https://github.com/Calebs1979/PowerShell_Repo/blob/main/PowerShell%20WinForm%20GUI%20Scripts/Logo.v1.png?raw=true"
+    $imageURL = "https://raw.githubusercontent.com/Calebs1979/PowerShell_Repo/main/PowerShell%20WinForm%20GUI%20Scripts/PowerShell%20Generator/Logo.v1.png"
     $image = [System.Net.WebRequest]::Create($imageUrl).GetResponse().GetResponseStream()
     $pictureBox.Image = [System.Drawing.Image]::FromStream($image)
     $pictureBox.SizeMode = "Stretch"  # Fit the image within the PictureBox
